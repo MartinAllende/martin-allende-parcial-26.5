bf06493acf0c4805d0f038882ef6e16bd5421d42
MartinAllende <martin.allende.2002@gmail.com>
2022-05-26 14:19:50 -0300

    parcial completo

---
+++ Parcial 26.5/src/Parcial 26.5.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vivienda.h"
+#include "censista.h"
+#include "validaciones.h"
+#include "funciones.h"
+
+#define TAM 5
+
+int main(void)
+{
+
+	setbuf(stdout,NULL);
+
+	pCensista censistas[3] =
+			{
+					{100, "Ana", 34, 12032345},
+					{101, "Juan", 24, 430154678},
+					{102, "Sol", 47, 590237487}
+			};
+
+	pVivienda viviendas[TAM];
+
+	int opcion;
+	int retorno;
+	int flag = 0;
+
+
+	inicializarViviendas(viviendas,TAM);
+
+	do
+	{
+		retorno = getNumero(&opcion,"ingrese su opcion\n1. Ingresar una vivienda\n2. Modificar una vivienda\n3. Eliminar una vivienda\n4. listar viviendas\n5. listar censistas\n6. salir\n",6,1);
+		if(retorno == 0)
+		{
+			switch(opcion)
+			{
+
+				case 1:
+					altaVivienda(viviendas,TAM,censistas,3);
+					flag = flag + 1;
+					break;
+				case 2:
+					if(flag != 0)
+					{
+						modificarVivienda(viviendas,TAM);
+					}
+					else
+					{
+						printf("Primero debe ingresar al menos una vivienda\n");
+					}
+					break;
+				case 3:
+					if(flag != 0)
+					{
+						bajaVivienda(viviendas,TAM);
+						flag = flag - 1;
+					}
+					else
+					{
+						printf("Primero debe ingresar al menos una vivienda\n");
+					}
+					break;
+				case 4:
+					if(flag != 0)
+					{
+						listarViviendas(viviendas,TAM);
+					}
+					else
+					{
+						printf("Primero debe ingresar al menos una vivienda\n");
+					}
+					break;
+				case 5:
+					listarCensistas(censistas,3);
+					break;
+
+			}
+		}
+		else
+		{
+			printf("error al ingresar su opcion\n");
+		}
+
+	}while(opcion != 6);
+
+
+
+
+
+
+
+
+
+}
+
